5a3478a348f8cc1d84add996b4fb6f4d8f253eab
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-02 15:48:38 -0600

    lesson DIY method - average_play_count

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,12 +2,32 @@
 
 class Song < ActiveRecord::Base
 
+  # This prevents us from creating a song if there's no title
+  #   validates :title,      presence: true
+  #   validates :length,     presence: true
+  #   validates :play_count, presence: true
+  # --same:
+  validates_presence_of :title, :length, :play_count
+
+
+  # --- Can also do this ---
+  # This allows you to chain other types of validations
+  #   validates :play_count, presence: true
+  # can also do this, but cannot chain
+  #   validates_presence_of :title
+
+
+
   # When you need to interact with any ?/subset? of instances,
   # must use class methods
   def self.total_play_count
     # ActiveRecord gives us sum in this context
     # ActiveRecord is turning this method into the much longer SQL code
     sum(:play_count)
+  end
+
+  def self.average_play_count
+    average(:play_count)
   end
 
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -11,6 +11,17 @@ RSpec.describe Song do
       # -- be_valid is a feature of rspec(?) and means valid instance of our class (Song)
     end
 
+    it 'should not be valid without a length' do
+      song = Song.create(title: "song_title", play_count: 100)
+      expect(song).to_not be_valid   #this is like minitest assert instance of
+      # -- be_valid is a feature of rspec(?) and means valid instance of our class (Song)
+    end
+
+    it 'should not be valid without a play_count' do
+      song = Song.create(title: "song title", length: 100)
+      expect(song).to_not be_valid   #this is like minitest assert instance of
+      # -- be_valid is a feature of rspec(?) and means valid instance of our class (Song)
+    end
 
   end
 
@@ -27,24 +38,25 @@ RSpec.describe Song do
       # context 'admin user'  # -- example of how to use this
       # IT --- this is where the test actually happens
       it 'returns the total play count for all songs' do
-
         # if we don't add a Teardown function, we'll keep adding these
         # songs to the database and affect our tests if run more than once.
         # Additionally, these are actually writing to our real database, which is undesirable.
         Song.create(title: "song 1", length: 100, play_count: 100)
         Song.create(title: "song 2", length: 200, play_count: 200)
-
-        count           = Song.total_play_count
-        expected_result = 300
-
-        # Assertion
-        expect(count).to eq(expected_result)
-
+        count    = Song.total_play_count
+        expected = 300
+        expect(count).to eq(expected)   # Assertion
       end
+    end
 
-
-
-
+    describe '.average_play_count' do
+      it 'returns the average play count for all songs' do
+        Song.create(title: "song 1", length: 100, play_count: 100)
+        Song.create(title: "song 2", length: 200, play_count: 200)
+        average  = Song.average_play_count
+        expected = 150
+        expect(average).to eq(expected)
+      end
     end
 
   end
