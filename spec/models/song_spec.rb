18d31f5f662c0417f9b6597d276560abfdc24539
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-02 14:42:35 -0600

    make spec tests, fix typo in spec_helper

---
+++ spec/models/song_spec.rb
@@ -0,0 +1,20 @@
+
+
+RSpec.describe Song do
+  describe 'Class methods' do
+
+    # Dream coding - Top Down - 'I want to be able to do this thing'
+    # At some point I want to be able to call Song.total_play_counts
+    # This is the name of the test -- should be useful to reader
+    describe '.total_play_counts' do
+      # context 'admin user'  # -- example of how to use this
+      # IT --- this is where the test actually happens
+      it 'returns the total play count for all songs' do
+
+      end
+
+    end
+
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 
 require 'bundler'
 
-Bundle.require(:default, :test)
+Bundler.require(:default, :test)
 # require default gems from Gemfile and then extra gems when you run tests
 # which you can see in the Gemfile
 require File.expand_path('../../config/environment.rb', __FILE__)
