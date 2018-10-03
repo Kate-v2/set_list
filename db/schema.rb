52d9422b5d85d7a44d5f3bd1897273958d19fc84
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-03 10:06:59 -0600

    created migration (rake db:migrate) which updates schema.rb and set-list-development

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_03_152520) do
+ActiveRecord::Schema.define(version: 2018_10_03_160409) do
 
   create_table "playlists", force: :cascade do |t|
     t.text "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_10_03_152520) do
     t.integer "play_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "playlist_id"
   end
 
 end
