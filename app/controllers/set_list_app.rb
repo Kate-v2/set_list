782dcd07b264954f65f999e4349ab1c47c1a0316
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-03 13:43:45 -0600

    Some notes on passing params and dynamic paths (in set_list_app.rb )

--- app/controllers/set_list_app.rb
+++ app/controllers/set_list_app.rb
@@ -5,6 +5,12 @@ class SetList < Sinatra::Base
     erb :"songs/index"
   end
 
+  # parameters come from the url
+  # http://localhost:9393/songs/2?term=apple
+  # params = { "term" => "apple", "id" => "2"}
+  # all params come in as strings
+  # a feature of Sinatra is that the type can be converted innately
+  #  --> params["id"]  =  params[:id]
   get '/songs/:id' do
     # @song = Song.find(1)
     @song = Song.find(params[:id])
