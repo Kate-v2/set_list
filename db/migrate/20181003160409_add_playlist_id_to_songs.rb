b148c5c13e3547502e55ac9ca88171f48af88871
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-03 10:05:52 -0600

    Update AddPlaylistIdtoSong migration template

--- db/migrate/20181003160409_add_playlist_id_to_songs.rb
+++ db/migrate/20181003160409_add_playlist_id_to_songs.rb
@@ -1,4 +1,7 @@
 class AddPlaylistIdToSongs < ActiveRecord::Migration[5.2]
+
   def change
+      add_column :songs, :playlist_id, :integer
+      # add col to songs, called playlist_id, of type integer
   end
 end
