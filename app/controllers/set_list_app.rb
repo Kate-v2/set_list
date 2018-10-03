6520344af2fc830fa86becde2710ea6a4e597768
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-03 13:59:06 -0600

    page was broken because of order with the wild card method, so re-order the new method and now the page works

--- app/controllers/set_list_app.rb
+++ app/controllers/set_list_app.rb
@@ -5,20 +5,31 @@ class SetList < Sinatra::Base
     erb :"songs/index"
   end
 
+    # PLCEMENT IS IMPORTANT
+    # when this came after the next method (get '/songs/:id')
+    # it thought new was the :id  id=new is not valid for any song
+    # so page breaks
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+
   # parameters come from the url
   # http://localhost:9393/songs/2?term=apple
   # params = { "term" => "apple", "id" => "2"}
   # all params come in as strings
   # a feature of Sinatra is that the type can be converted innately
   #  --> params["id"]  =  params[:id]
+  # here in the path :id is a "wild card"
   get '/songs/:id' do
     # @song = Song.find(1)
     @song = Song.find(params[:id])
     erb :"songs/show"
   end
 
-  get '/songs/new' do
-    erb: :'songs/new'
-  end
+  # PLCEMENT IS IMPORTANT
+  # --- MOVED ---
+  # get '/songs/new' do
+  #   erb: :'songs/new'
+  # end
 
 end
