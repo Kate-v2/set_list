49030507f1aa7c25577f83b8b36fa9e7aeced695
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-03 10:27:37 -0600

    ran rake db:migrate (add changes from playlist.rb)

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -5,6 +5,6 @@ class Playlist < ActiveRecord::Base
   # one Playlist has many songs        -- one:many  -- has_many
   # one song belongs to one Playlist   -- one:one   -- belongs_to
 
-
+  has_many :songs
 
 end
