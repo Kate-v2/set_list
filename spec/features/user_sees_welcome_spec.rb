c9d1fd4f2ba8794d4233eb8510f6772b8ae425a6
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-04 10:25:30 -0600

    notes about launchy

--- spec/features/user_sees_welcome_spec.rb
+++ spec/features/user_sees_welcome_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "a visitor visits the welcome page" do
   it 'should show a welcome message' do
     visit '/'
     # save_and_open_page  # launchy command -- saved in tmp folder -- this is a debug tool like pry
+    #                     this won't display your css (unless you're pulling from like live/cloud hosted css in your head -- you're probably not)
     within('#greeting') do
       expect(page).to have_content('Welcome!')
     end
