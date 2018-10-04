4d165e078f4d4f06cceeacf6801576777846eb04
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-04 10:10:24 -0600

    add a feature test

---
+++ spec/features/user_sees_welcome_spec.rb
@@ -0,0 +1,20 @@
+
+
+
+RSpec.describe "a visitor visits the welcome page" do
+
+  it 'should show a welcome message' do
+    visit '/'
+
+    within('#greeting') do
+      expect(page).to have_content('Welcome!')
+    end
+  end
+
+end
+
+
+# Can write a test like a user story
+#   describe 'as a visitor'
+#     describe 'when I visit the welcome page'
+#       it 'should show a welcome message'
