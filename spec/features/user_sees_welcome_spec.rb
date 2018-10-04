a10ad1acacb9d177cfd7567b9e0563f93258fe4a
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-04 10:22:47 -0600

    learned how to use launchy gem - save_and_open_page

--- spec/features/user_sees_welcome_spec.rb
+++ spec/features/user_sees_welcome_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "a visitor visits the welcome page" do
 
   it 'should show a welcome message' do
     visit '/'
-
+    # save_and_open_page  # launchy command -- saved in tmp folder -- this is a debug tool like pry
     within('#greeting') do
       expect(page).to have_content('Welcome!')
     end
